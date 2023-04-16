65adf5e2262e70663e90cf1f92be525965723568
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-04-16 16:23:39 -0400

    Criação das tabelas

--- scripts/tp1_3.2.py
+++ scripts/tp1_3.2.py
@@ -1,32 +1,92 @@
 import psycopg2
+import sys
 
 params = "dbname=postgres host=localhost user=postgres password=postgres port=5432"
-
-
-def create_tables():
-    """ create tables in the PostgreSQL database"""
-    commands = (
+tables = ("reviews","produtoSimilar","produtoCategoria","categoria","produto")
+commands = (
         """
-           TABELA 1
+           CREATE TABLE produto (
+                asin VARCHAR(10) PRIMARY KEY,
+                title VARCHAR(470) NOT NULL,
+                pgroup varchar(30) NOT NULL,
+                salesrank INTEGER NOT NULL
+           )
         """,
         """ 
-           TABELA 2
+           CREATE TABLE categoria(
+             id INTEGER PRIMARY KEY,
+             name VARCHAR(250)
+           )
         """,
         """
-            TABELA 3
+           CREATE TABLE produtoCategoria(
+                id INTEGER not NULL,
+                sequencia INTEGER NOT NULL,
+                asinproduto VARCHAR(10),
+                id_categoria INTEGER NOT null,
+                primary key (id,sequencia),
+                CONSTRAINT fk_prodcateg_asinproduto
+                    FOREIGN KEY(asinproduto)
+                            REFERENCES produto (asin),
+                
+                CONSTRAINT fk_prodcateg_categoria
+                    FOREIGN KEY(id_categoria)
+                            REFERENCES categoria (id)
+           )
+
         """,
         """
-            TABELA 4
+            CREATE TABLE produtoSimilar(
+              id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+              asinproduto VARCHAR(10),
+              asinsimilar VARCHAR(10),
+              CONSTRAINT fk_prodsim_asinproduto
+	              FOREIGN KEY(asinproduto)
+	                    REFERENCES produto (asin),
+	          
+              CONSTRAINT fk_prodsim_asinsimilar
+	              FOREIGN KEY(asinsimilar)
+	                    REFERENCES produto (asin)
+	              
+           )
+
         """,
         """
-            TABELA 5
-        """,
-        """
-            TABELA 6
-        """,
-        """
-            TABELA 7
+            CREATE TABLE reviews (
+              id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+              asinproduto VARCHAR(10),
+              data DATE DEFAULT CURRENT_DATE,
+              cliente VARCHAR(20) not null,
+              classficacao INTEGER DEFAULT 0,
+              votos INTEGER DEFAULT 0, 
+              util INTEGER DEFAULT 0,
+              CONSTRAINT fk_reviews_asinproduto
+	              FOREIGN KEY(asinproduto)
+	                    REFERENCES produto (asin)
+	          
+            )
         """)
+def remove_tables():
+    try:
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(params)
+        cur = conn.cursor()
+        # create table one by one
+        for table in tables:
+            #print("DROP TABLE IF EXISTS %s",table)
+            cur.execute("DROP TABLE IF EXISTS "+table+" ")
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        
+def create_tables():
+    """ create tables in the PostgreSQL database"""
     conn = None
     try:
         # connect to the PostgreSQL server
@@ -98,11 +158,15 @@ def read_entry(entryName):
         print("Group: ", group)
         print("Sales Rank: ", salesrank)            
         print("Similares: ",similar)
-        print("Categorories: ",categories)
+        print("Categories: ",categories)
         print("Reviews: ", reviews)
         
 
 
     print()
 if __name__ == '__main__':
-    read_entry("teste.txt")
+    fileName = sys.argv[1]
+    #print(fileName)
+    read_entry(fileName)
+    #remove_tables()
+    #create_tables()
