9be108334f64011a7af3afb67914fa87b88e44ef
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-04-17 20:12:40 -0400

    Sql do dashboard, ajuste no process. de entrada

--- scripts/tp1_3.2.py
+++ scripts/tp1_3.2.py
@@ -1,6 +1,7 @@
 import psycopg2
 import sys
-
+import gc
+import time
 params = "dbname=postgres host=localhost user=postgres password=postgres port=5432"
 tables = ("reviews","produtoSimilar","produtoCategoria","categoria","produto")
 similars = []
@@ -47,10 +48,6 @@ commands = (
               asinsimilar VARCHAR(10),
               CONSTRAINT fk_prodsim_asinproduto
 	              FOREIGN KEY(asinproduto)
-	                    REFERENCES produto (asin),
-	          
-              CONSTRAINT fk_prodsim_asinsimilar
-	              FOREIGN KEY(asinsimilar)
 	                    REFERENCES produto (asin)
 	              
            )
@@ -76,6 +73,7 @@ commands = (
 def remove_tables():
     try:
         # connect to the PostgreSQL server
+        print("Removing tables created before")
         conn = psycopg2.connect(params)
         cur = conn.cursor()
         # create table one by one
@@ -91,6 +89,8 @@ def remove_tables():
     finally:
         if conn is not None:
             conn.close()
+        print("Tables Removed")
+        
        
 def create_tables():
     """ create tables in the PostgreSQL database"""
@@ -100,6 +100,7 @@ def create_tables():
         conn = psycopg2.connect(params)
         cur = conn.cursor()
         # create table one by one
+        print("Creating Tables")
         for command in commands:
             cur.execute(command)
         # close communication with the PostgreSQL database server
@@ -111,43 +112,34 @@ def create_tables():
     finally:
         if conn is not None:
             conn.close()
-
-def add_produto(asin,group,title,salesrank):
-    try:
-        conn = psycopg2.connect(params)
-        cur = conn.cursor()
-        # create table one by one
-        command = "INSERT INTO produto(asin,pgroup,title,salesrank) values ('"+asin+"', '"+group+"','"+title+"',"+salesrank+") ON CONFLICT (asin) DO NOTHING"
-        #print(command)
-        cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+        print("Tables Created")
 
 def process_similar():
     try:
         conn = psycopg2.connect(params)
         cur = conn.cursor()
         # create table one by one
-        for nsimilar in similars:
+        command = ""
+        icount = 0
+        for count,nsimilar in enumerate(similars,start=1):
             if nsimilar != []: 
                 for item in nsimilar[1::] :
                     #print(nsimilar)
-                    command = "INSERT INTO produtoSimilar(asinproduto,asinsimilar) values ('"+nsimilar[0]+"','"+item+"') ON CONFLICT (id) DO NOTHING"
-                    #print(command)
-                    cur.execute(command)
+                    command += "INSERT INTO produtoSimilar(asinproduto,asinsimilar) values ('"+nsimilar[0]+"','"+item+"');\n"
+                    print("Processing similars: ",count,item)
+                    icount+=1
+                    if((icount)%50000==0):
+                        cur.execute(command)
+                        command=""
                 # close communication with the PostgreSQL database server
+        if command != "":
+            cur.execute(command)
         cur.close()
         # commit the changes
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        sys.exit()
     finally:
         if conn is not None:
             conn.close()
@@ -156,8 +148,10 @@ def process_reviews():
     try:
         conn = psycopg2.connect(params)
         cur = conn.cursor()
+        conn.autocommit=True
         # create table one by one
-        for x in nreviews:
+        command=""
+        for count,x in enumerate(nreviews,start=1):
             #print(x)
             if x != []: 
                 asin= x[0]
@@ -167,13 +161,21 @@ def process_reviews():
                 votos= x[4]
                 util = x[5]
                 #print(item)
-                command = "INSERT INTO reviews(asinproduto,data,cliente,classficacao,votos,util) values ('"+asin+"','"+data+"','"+cliente+"',"+classificacao+","+votos+","+util+")"
-                cur.execute(command)
+                command += "INSERT INTO reviews(asinproduto,data,cliente,classficacao,votos,util) values ('"+asin+"','"+data+"','"+cliente+"',"+classificacao+","+votos+","+util+");\n"
+                print("Process Reviews:", count)
+                if(count%50000 == 0):
+                    print("Executing")
+                    cur.execute(command)
+                    command = ""                    
+        cur.execute(command)
         cur.close()
         # commit the changes
         conn.commit()
+        nreviews.clear();
+        gc.collect()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        sys.exit()
     finally:
         if conn is not None:
             conn.close()
@@ -181,22 +183,27 @@ def process_reviews():
 def process_products():
     try:
         conn = psycopg2.connect(params)
+        conn.autocommit = True
         cur = conn.cursor()
-        for product in nproducts:
+        command = ""
+        for count,product in enumerate(nproducts,start=1):
             asin = product[0]
             group = product[1]
             title = product[2]
             salesrank = product[3]
             # create table one by one
-            command = "INSERT INTO produto(asin,pgroup,title,salesrank) values ('"+asin+"', '"+group+"','"+title+"',"+salesrank+") ON CONFLICT (asin) DO NOTHING"
-            #print(command)
-            cur.execute(command)
+            command+= "INSERT INTO produto(asin,pgroup,title,salesrank) values ('"+asin+"', '"+group+"','"+title+"',"+salesrank+");\n"
+            #print("Process Product:",count)
             # close communication with the PostgreSQL database server
+        cur.execute(command)
         cur.close()
         # commit the changes
         conn.commit()
+        nproducts.clear()
+        gc.collect()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        sys.exit()
     finally:
         if conn is not None:
             conn.close()
@@ -206,26 +213,36 @@ def process_categories():
         conn = psycopg2.connect(params)
         cur = conn.cursor()
         # create table one by one
+        command = ""
+        count = 0
         for categories in ncategories:
             #print(categories)
             asin=categories[0]
             for index,item in enumerate(categories[1:],start=1) :
                 #print(item)
                 items = item.split('[')
-                #print(items)
-                command = "INSERT INTO categoria(id,name) values ("+items[1].replace("]","")+", '"+items[0]+"') ON CONFLICT (id) DO NOTHING"
-                #print(command)
-                cur.execute(command)
-                command = "INSERT INTO produtoCategoria(id,sequencia,asinproduto,id_categoria) values ("+str(len(idCateg))+","+str(index)+",'"+asin+"',"+items[1].replace("]","")+") ON CONFLICT (id,sequencia) DO NOTHING"
-                #print(command)
-                cur.execute(command)
-        # close communication with the PostgreSQL database server
+                command += "INSERT INTO categoria(id,name) values ("+items[-1].replace("]","")+", '"+items[0]+"') ON CONFLICT (id) DO NOTHING;; \n"
+                command += "INSERT INTO produtoCategoria(id,sequencia,asinproduto,id_categoria) values ("+str(len(idCateg))+","+str(index)+",'"+asin+"',"+items[-1].replace("]","")+");\n"
+                print("Processing Categories: ",count)
+                count+=2  
+                if ((count%50000) == 0):
+                    print("Executing")
+                    cur.execute(command)
+                    count = 0
+                    #time.sleep(10)
+                    command="" 
             idCateg.append(len(idCateg)+1)
+            #print(idCateg)
+            #time.sleep(10)
+        cur.execute(command)    
         cur.close()
         # commit the changes
         conn.commit()
+        ncategories.clear()
+        gc.collect()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        sys.exit()
     finally:
         if conn is not None:
             conn.close()
@@ -235,9 +252,11 @@ def process_categories():
 def read_entry(entryName):
     arq = open(entryName,"r")
     data = arq.read();
-    dataList = data.split('\n\n')
-    for ndata in dataList:
+    dataList = data.split('\nId:   ')
+    #print(dataList)
+    for count,ndata in enumerate(dataList,start=1):
         #print(ndata)
+        #print("")
         #print('--------------------------------------------------------------------------------')
         lines = ndata.split('\n');
         #print(lines)
@@ -245,9 +264,6 @@ def read_entry(entryName):
         asin = ""
         group = ""
         salesrank = ""
-        similar = []
-        categories = []
-        reviews=[]
         for line in lines:
             if ']|' in line:
                 line = line.replace("'","''")
@@ -255,7 +271,7 @@ def read_entry(entryName):
                 ncategories.append([asin]+info[1::])
             elif 'title' in line:
                     info = line.split(' ',3)
-                    #print(info)
+                    #print(line)
                     while '' in info:
                         info.remove('')
                     title = info[1]
@@ -269,37 +285,65 @@ def read_entry(entryName):
                 
                 #####
                 
-                if 'ASIN' in line:
+                if 'ASIN:' in line:
+                    print(line)
                     asin = info[1]
-                if 'group' in line:
+                if 'group:' in line:
                     group = info[1]
-                if 'salesrank' in line:
+                if 'salesrank:' in line:
                     salesrank = info[1]
-                if 'similar' in line:
-                    similar.append(asin)
-                    for n in info[2:]:
-                        similar.append(n)
-                if 'cutomer' in line:
+                if 'similar:' in line:
+                    #similar.append(asin)
+                    #for n in info[2:]:
+                    #    similar.append(n)
+                    similars.append([asin]+info[2::])
+                if 'cutomer:' in line:
                     nreviews.append([asin]+info[0::2])
-                
+        print("Product:",count)  
+        """if (asin=='B00004YWW7') or (asin == 'cutomer:'):
+                print(ndata);
+                #print(nproducts)
+                sys.exit()    
+         """   
         if title != '':
             title=title.replace("'","''") 
             nproducts.append([asin,group,title,salesrank])
-            similars.append(similar)
+            #similars.append(similar)
             #nreviews.append()
+            if len(nproducts)==20000:
+               process_products()     
+               process_reviews()
+               process_categories()
+               gc.collect()
+
+        #print(asin)
+        #print(title)
+        #print(group)
+        #print(salesrank)
+        #print(nproducts)
+        #print(nreviews)
+
+
+                       
+     #  process_reviews()
+     #          nreviews.clear() 
     print()
 
 if __name__ == '__main__':
     fileName = sys.argv[1]
+    tinicio = time.time()
     print(fileName)
     remove_tables()
     create_tables()
     read_entry(fileName)
-    #print("Incluindo Produtos")
+    print("Incluindo Produtos")
     process_products()
-    #print(Incluindo Reviews)
+    print("Incluindo Reviews")
     process_reviews()
-    #print(Incluindo Produtos Similares)
+    print("Incluindo Produtos Similares")
+    print(similars)
     process_similar()
-    #print(Incluindo Produtos Categorias e subcategorias)
+    print("Incluindo Produtos Categorias e subcategorias")
     process_categories()
+    tfim = time.time()
+    print("Tempo de execução:",(tfim-tinicio,"segundos"))
--- scripts/tp1_3.3.py
+++ scripts/tp1_3.3.py
@@ -2,17 +2,24 @@ import psycopg2
 
 params = "dbname=postgres host=localhost user=postgres password=postgres port=5432"
 
-
+codprod = ""
 commands = (
         """
-           Consulta 1
+           (select r.* from reviews r where r.asinproduto = %s order by r.util desc,r.votos desc limit 5)
+            union all
+           (select r.* from reviews r where r.asinproduto = %s order by r.util desc,r.votos asc limit 5) 
+
         """,
         """ 
-           Consulta 2
+            select p.* from produtosimilar p2 
+            left join produto p on p.asin = p2.asinsimilar 
+            left join produto p4 on p4.asin = p2.asinproduto 
+            where p2.asinproduto = %s and p.salesrank < (select p3.salesrank from produto p3 where asin = p2.asinproduto)
+
         """,
         """
             Consulta 3
-        """,
+        """, 
         """
             Consulta 4
         """,
@@ -25,12 +32,16 @@ commands = (
         """
             Consulta 7
         """)
-def get_info(commandCode : int):
+
+def get_info(commandCode : int,param):
     conn = None
     try:
         conn = psycopg2.connect(params) 
         cur = conn.cursor();
-        cur.execute(commands[commandCode-1])
+        command = commands[commandCode-1]
+        if (commandCode in [1,2,3]):
+            command = command.replace("%s","'"+str(param)+"'")
+        cur.execute(command,{param,param})
         
         row = cur.fetchone()
         while row is not None:
@@ -57,5 +68,9 @@ if __name__ == '__main__':
     print("6. Listar a 5 categorias de produto com a maior média de avaliações úteis positivas por produto")
     print("7. Listar os 10 clientes que mais fizeram comentários por grupo de produto")
     print()
-    option = input("Digite a opção desejada: ")
-
+    option = int(input("Digite a opção desejada: "))
+    param = ""
+    print("Opção",str(option),"selecionada")
+    if option in [1,2,3]:
+        param = input("Produto desejado: ")
+    get_info(int(option),param)
